e35b826ccb010a287dcef49c743aa215ddbec890
Endrick Rodrigues <endrickrodrigues2011@gmail.com>
2022-01-20 15:10:36 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/endrick/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.endrick.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.endrick.workshopmongo.domain.User;
+import com.endrick.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
